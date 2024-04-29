5576403a9125ef10f7419c073507590e102b8ec7
Anders Mumm <andersmumm@gmail.com>
2024-04-29 12:09:01 +0200

    Co-authored-by: MarcoFlyt <MarcoFlyt@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-for i in range(0,3):
-	index1 = int((90 / 3) * i)
-	index2 = int((90 / 3) * (i + 1))
+def extend_list_to_360(lst):
+    new_list = lst[:]  # Copy the original list
+    while len(new_list) < 360:
+        for i in range(len(new_list) - 1):
+            new_element = (new_list[i] + new_list[i + 1]) / 2
+            new_list.insert(i + 1, new_element)
+            if len(new_list) >= 360:
+                break
+    return new_list[:360]
 
-	print(index1)
-	print(index2)
-	print()
+# Example usage:
+original_list = [1, 2, 3, 4, 5]  # Change this list to whatever you want
+extended_list = extend_list_to_360(original_list)
+print(extended_list)
+print(len(extended_list))  # Ensure the length of the list is 360
